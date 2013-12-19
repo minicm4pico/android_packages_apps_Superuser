4599ba00d6ee9ee0b353062ee51b189927106a51
Vineeth Raj <vineethraj49@gmail.com>
2013-12-19 21:57:47 +0530

    Add arity lib.
    Superuser compilation needn't be dependant on "Calculator" app compilation.
    
    Change-Id: I8e2310a566e6949e0b80065119f1ab63ad6a5c30

--- Android.mk
+++ Android.mk
@@ -2,6 +2,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_STATIC_JAVA_LIBRARIES := libarity android-support-v4
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libarity:arity-2.1.2.jar
+
 LOCAL_PACKAGE_NAME := Superuser
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
@@ -10,7 +13,17 @@ LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
 LOCAL_CERTIFICATE := superuser
 
+LOCAL_DEX_PREOPT := false
+
 include $(BUILD_PACKAGE)
 
 ##
 
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libarity:arity-2.1.2.jar
+
+include $(BUILD_MULTI_PREBUILT)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
